b727e5245498014fde357684f8ab5ba786ffc88b
Sirspam <Charlespattison555@Gmail.com>
2021-05-06 12:44:42 +0100

    Added more random images to neko

--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,7 @@
 import discord
 import logging
 from random import getrandbits
+from random import choice
 from discord.ext import commands
 from io import BytesIO
 
@@ -27,8 +28,10 @@ class General(commands.Cog):
 
     @commands.command(hidden=True)
     async def neko(self, ctx):
-        # Add a check to see if neko cog is loaded
-        async with self.bot.session.get("https://cdn.discordapp.com/emojis/786584088403509338.png") as resp:
+        # Hmm yes, hard corded database B)
+        async with self.bot.session.get(choice(["https://cdn.discordapp.com/emojis/786584088403509338.png",
+        "https://cdn.discordapp.com/attachments/587749898729029648/839806693621760000/Untitled138_20210505215431.png",
+        "https://cdn.discordapp.com/attachments/587749898729029648/839807192601198602/Untitled138_20210505210112.png"])) as resp:
                 await ctx.reply(file=discord.File(BytesIO(await resp.read()), "cute_kawaii_neko.png"))
 
 def setup(bot):

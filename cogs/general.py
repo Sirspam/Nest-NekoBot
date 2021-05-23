c3c360f17383bf7cdc379ae3f3dbacd2676ab49e
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-05-23 12:05:44 +0100

    Disabled jaydeez nuts
    Today is a sad day

--- cogs/general.py
+++ cogs/general.py
@@ -17,6 +17,7 @@ class General(commands.Cog):
         if message.author == self.bot.user:
             return
         if "jaydz" in message.content.lower():
+            return # utter sadness
             if randint(0, 9) == 0:
                 await message.reply("jaydeez nuts <:Jaydz1Tf:840275619325673553>")
                 logging.info("Posted jaydeez nuts :tf:")

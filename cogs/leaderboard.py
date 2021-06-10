489b5857ea750ab84f3357bd0a61963ac6d624d0
Sirspam <Charlespattison555@Gmail.com>
2021-06-10 09:47:19 +0100

    Added menu to leaderboard

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -1,95 +1,35 @@
 import discord
 import logging
 from discord.ext import commands
-#from discord.ext import menus
+from discord.ext import menus
 from discord.ext.commands.errors import BadArgument
 from firebase_admin import firestore
 from utils import ranking_roles
 
 
 dab = firestore.client()
-"""
-class LeaderboardMenu(menus.Menu):
-    #def __init__(self):
-        #return
-    
-    async def send_initial_message(self, ctx, type, channel):
-        logging.info("leaderboard invoked")
-        segregated_leaderboard = list()
-        if type.lower()=="modded":
-            logging.info("modded type")
-            type_collectionlist = dab.collection("users").document("collectionlist").get().get("modded")
-            for x in self.bot.leaderboard:
-                if x[0] in type_collectionlist:
-                    segregated_leaderboard.append(x)
-            title = "(Modded)"
-        elif type.lower()=="quest":
-            logging.info("quest type")
-            type_collectionlist = dab.collection("users").document("collectionlist").get().get("quest")
-            for x in self.bot.leaderboard:
-                if x[0] in type_collectionlist:
-                    segregated_leaderboard.append(x)
-            title = "(Quest)"
-        elif type.lower()=="all":
-            logging.info("all type")
-            segregated_leaderboard=self.bot.leaderboard
-            title = "(All)"
-        else:
-            raise BadArgument
-        count = 1
-        page_count = 0 
-        pages = list()
-        message = str()
-        for x in segregated_leaderboard:
-            if page_count == 15:
-                pages.append(message)
-                message = str()
-                page_count = 0
-                #print(pages)
-            member = ctx.guild.get_member(int(x[0]))
-            if member is None:
-                continue
-            message = message+f"{await ranking_roles.return_emote(x[2])} #{count} - ``{member.name}`` - MP: {x[1]}\n"
-            count = count+1
-            page_count = page_count+1
-        embed = discord.Embed(
-            title = f"Nest Multi Ranking Leaderboard {title}",
-            description = pages[0],
-            colour = 0xfc0000,
-            timestamp=ctx.message.created_at
+
+class LeaderboardMenu(menus.ListPageSource):
+    def __init__(self, data, embed):
+        super().__init__(data, per_page=15)
+        self.embed = embed
+
+    async def format_page(self, menu, entries):
+        offset = menu.current_page * self.per_page
+        self.embed.clear_fields()
+        self.embed.add_field(
+            name="\u200b",
+            value='\n'.join(f"#{i+1} <@{v[0]}> MP: **{v[1]}** {v[2]}" for i, v in enumerate(entries, start=offset)),
+            inline=True
         )
-        embed.set_thumbnail(url=(ctx.guild.get_member(int(segregated_leaderboard[0][0]))).avatar_url)
-        return await ctx.send(embed=embed)
+        self.embed.set_footer(text=f"Page {(menu.current_page+1)}/{self.get_max_pages()}")
+        return self.embed
 
-
-    @menus.button("\N{BLACK LEFT-POINTING TRIANGLE}")
-    async def on_back(self, payload):
-        embed = discord.Embed(
-            title = f"Nest Multi Ranking Leaderboard {title}",
-            description = pages[0],
-            colour = 0xfc0000,
-            timestamp=self.ctx.message.created_at
-        )
-        await self.message.edit(content="cunt")
-
-    @menus.button("\N{BLACK RIGHT-POINTING TRIANGLE}")
-    async def on_forward(self, payload):
-        await self.message.edit(content="nonce")
-
-    @menus.button("\N{BLACK SQUARE FOR STOP}")
-    async def on_stop(self, payload):
-        self.stop()
-"""
 
 class Leaderboard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-    #@commands.command()
-    #async def cringe(self, ctx):
-    #    type = "All"
-    #    await LeaderboardMenu().start(ctx, wait=True)
     
     @commands.command(help="Posts a leaderboard of the individiuals with the most MP\nValid arguments are ``all``, ``modded`` and ``quest``",aliases=["lb"])
     async def leaderboard(self, ctx, type="all"):
@@ -115,32 +55,14 @@ class Leaderboard(commands.Cog):
             title = "(All)"
         else:
             raise BadArgument
-        count = 1
-        page_count = 0 
-        pages = list()
-        message = str()
-        for x in segregated_leaderboard:
-            if page_count == 15:
-                pages.append(message)
-                message = str()
-                page_count = 0
-                #print(pages)
-            member = ctx.guild.get_member(int(x[0]))
-            if member is None:
-                continue
-            message = message+f"{x[2]} #{count} - ``{member.name}`` - MP: {x[1]}\n"
-            count = count+1
-            page_count = page_count+1
-        if pages == []:
-            pages.append(message)
         embed = discord.Embed(
             title = f"Nest Multi Ranking Leaderboard {title}",
-            description = pages[0],
             colour = 0xfc0000,
             timestamp=ctx.message.created_at
         )
         embed.set_thumbnail(url=(ctx.guild.get_member(int(segregated_leaderboard[0][0]))).avatar_url)
-        await ctx.send(embed=embed)
+        pages = menus.MenuPages(source=LeaderboardMenu(segregated_leaderboard, embed), timeout=30.0, clear_reactions_after=True)
+        await pages.start(ctx)
 
     @commands.command(hidden=True, help="Generates the leaderboard from firebase (debug)")
     @commands.has_any_role(*[769117646280982538,587963873186021376])

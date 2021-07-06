1d67bffe0e1d9e62ad08039f19bfa0bcc9a51266
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-07-06 20:47:04 +0100

    Added announcements :tf: reaction

--- cogs/general.py
+++ cogs/general.py
@@ -21,6 +21,8 @@ class General(commands.Cog):
             if randint(0, 9) == 0:
                 await message.reply("jaydeez nuts <:Jaydz1Tf:840275619325673553>")
                 logging.info("Posted jaydeez nuts :tf:")
+        if message.channel.id == 588471682570649641:
+            await message.add_reaction("<:tf:808417609732849664>")
 
     @commands.command(hidden=True)
     async def neko(self, ctx):

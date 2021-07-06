4fbb01a1708ea98d56707f9e2a8d644855b018f7
Sirspam <Charlespattison555@Gmail.com>
2021-07-06 14:57:34 +0100

    Improved statuses

--- cogs/status.py
+++ cogs/status.py
@@ -1,7 +1,9 @@
-import discord
 import logging
-from discord.ext import commands, tasks
+
+from discord import Game, Activity, ActivityType
 from random import choice, getrandbits
+
+from discord.ext import commands, tasks
 
 
 class Status(commands.Cog):
@@ -17,23 +19,17 @@ class Status(commands.Cog):
                 "Beat Saber",
                 "Shiny Happy Days on loop",
                 "with Sirspam's final braincell",
-                f"with {self.bot.get_guild(587749898045095960).member_count} people",
                 "with nekos",
                 "with booba 😳",
                 "😎"
             ])
-            await self.bot.change_presence(activity=discord.Game(name=value))
+            await self.bot.change_presence(activity=Game(name=value))
             logging.info(f"Status set to: {value}")
         else:
             value = choice([ # Watching
+                f"{self.bot.get_guild(587749898045095960).member_count} people"
                 "Jaydz live on twitch",
-                "Bitz live on Twitch :PeepoStare:",
-                "Sirspam live on Twitch",
-                "Soberra live on Twitch",
-                "Sckuffles live on Twitch",
-                "SomeTallDude on YouTube",
-                "for .matchstart",
-                "You",
+                "You.",
                 "Dan make a fool of himself",
                 "Soberra throwing a fit",
                 "Dan p :tf:",
@@ -42,13 +38,12 @@ class Status(commands.Cog):
                 "Carl's Jr get banned again",
                 "Bitz desperately rolling for waifus",
                 "Goose ad sddufba",
-                "Monke on youtube",
                 "Skip skip skippety skip skip skip",
                 "Bitz suggest awful statuses",
                 "Mar fangirl over Jaydz",
                 "Pigeon waste her primogems"
             ])
-            await self.bot.change_presence(activity=discord.Activity(name=value, type=discord.ActivityType.watching))
+            await self.bot.change_presence(activity=Activity(name=value, type=ActivityType.watching))
             logging.info(f"Status set to: {value}")
 
     @commands.Cog.listener()

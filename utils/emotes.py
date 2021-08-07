12aaf824eb0dc432d2ef2c4609b1af1bb00f38b8
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-08-07 23:56:00 +0100

    Added emotes.py

---
+++ utils/emotes.py
@@ -0,0 +1,15 @@
+diff_emotes = {
+    "Easy": "``E``",
+    "Normal": "``N``",
+    "Hard": "``H``",
+    "Expert": "``Ex``",
+    "ExpertPlus": "``Ex+``",
+}
+
+diff_stats_emotes = {
+    "star": "⭐",
+    "notes": "notes",
+    "bombs": "bombs",
+    "nps": "nps",
+    "njs": "njs"
+}

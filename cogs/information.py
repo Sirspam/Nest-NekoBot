929b414dfdd440e9d1b3da55076a4c6f791d22f9
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-08-08 00:03:00 +0100

    Update information.py

--- cogs/information.py
+++ cogs/information.py
@@ -1,7 +1,7 @@
-from discord import Embed, Permissions
+from discord import Embed, Colour
 
 from discord.ext import commands
-from discord.utils import oauth_url
+
 
 class Information(commands.Cog):
     "Informational related commands "
@@ -11,23 +11,13 @@ class Information(commands.Cog):
 
     @commands.command(aliases=["invite"], help="Posts links relevant to the bot")
     async def links(self, ctx):
-        permission_names = (
-            "send_messages",
-            "embed_links",
-            "attach_files",
-            "add_reactions",
-            "use_external_emojis"
-        )
-        perms = Permissions()
-        perms.update(**dict.fromkeys(permission_names, True))
         embed = Embed(
             description=
-f"""[**Bot Invite Link**]({oauth_url(self.bot.user.id, perms)})
-[**Home Server**](https://discord.gg/dWX6fpGUK9)
+f"""[**Home Server**](https://discord.gg/zTCJh8H)
 [**Github Repo**]({self.bot.github_repo})\n
 I hope you're having a good day :)""",
-            color=0x00A9E0)
-        embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/822087750798016552.gif?v=1")
+            color= Colour.red())
+        embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/765183849876684810.gif?v=1")
         await ctx.reply(embed=embed)
 
 
